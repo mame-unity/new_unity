34cee8c48acb86d56f519f4d51058e9ec1d643b4
mame0622 <mame.like.ww@gmail.com>
2022-12-12 11:59:58 +0900

    test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,9 @@ public class Player : MonoBehaviour
 
     private CharacterController controller;
 
+    private float horizontalSpeed;  // 水平移動速度
+    private float verticalSpeed;    // 垂直方向移動
+
     // フレーム更新処理の前に一度だけ呼ばれる関数
     void Start()
     {
@@ -20,14 +23,52 @@ public class Player : MonoBehaviour
     // 毎フレーム更新処理
     void Update()
     {
+        // 進行方向更新処理
+        UpdateDirection();
+
+        // 移動更新処理
+        UpdateMovement();
+    }
+
+    // 進行方向更新処理
+    private void UpdateDirection()
+    {
         // 水平方向入力情報取得
         float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // 水平軸入力情報が一定以上なら移動中
+        float power = Mathf.Abs(horizontal);
+        if(power>0.1f)
+        {
+            // 水平軸入力情報から進行方向を設定
+            Vector3 direction = new Vector3(horizontal, 0, 0);
+
+            // 進行方向に向くように回転設定
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+        power = Mathf.Abs(vertical);
+        if(power>0.1f)
+        {
+            // 水平軸入力情報から進行方向を設定
+            Vector3 direction = new Vector3(vertical, 0, 0);
+
+            // 進行方向に向くように回転設定
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+
+        // 移動速度
+        horizontalSpeed = horizontal * maxMoveSpeed;
+        verticalSpeed = vertical * maxMoveSpeed;
+    }
+
+    // 移動更新処理
+    private void UpdateMovement()
+    {
+        // 移動量を計算
+        Vector3 move = new Vector3(horizontalSpeed, 0, verticalSpeed);
 
         // 移動処理
-        Vector3 move;
-        move.x = horizontal * maxMoveSpeed;
-        move.y = 0.0f;
-        move.z = 0.0f;
         controller.Move(move * Time.deltaTime);
     }
 }
